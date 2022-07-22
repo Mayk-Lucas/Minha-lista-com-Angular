09eb79181eaf6933a44cfee0663bd0a1abc7e5d2
Mayk Lucas <maykads2018@gmail.com>
2022-07-22 10:23:54 -0300

    measuring in the data listing component

--- src/app/modules/home/components/list/list.component.ts
+++ src/app/modules/home/components/list/list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+//Interface
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+
+  public taskList: Array<TaskList> = [
+    { task: "Minha nova Task", checked: true },
+    { task: "Minha nova Task 2", checked: false }
+  ];
 
   constructor() { }
 
