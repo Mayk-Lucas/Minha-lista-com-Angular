6d1ba282f242d0fe0bc3888591093196a103006d
Mayk Lucas <maykads2018@gmail.com>
2022-07-25 09:06:59 -0300

    Creating the method to clear the list

--- src/app/modules/home/components/list/list.component.ts
+++ src/app/modules/home/components/list/list.component.ts
@@ -10,14 +10,22 @@ import { TaskList } from '../../model/task-list';
 })
 export class ListComponent implements OnInit {
 
-  public taskList: Array<TaskList> = [
-    { task: "Minha nova Task", checked: true },
-    { task: "Minha nova Task 2", checked: false }
-  ];
+  public taskList: Array<TaskList> = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList() {
+    const confirm = window.confirm("Você deseja realemnte Deletar tudo?");
+
+    if (confirm) {
+      this.taskList = [];
+    }
+  }
 }
